d3432ff1e142801736faef3ceebe7681a9787c3e
Diogo Nóbrega <diogonrl@hotmail.com>
2023-04-06 23:49:55 -0300

    MKT-01 | Create Product

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,8 +1,38 @@
 import slugify from "slugify"
+import fs from "fs";
 import Product from "../models/product.js"
 
 export const create = async (req, res) => {
     try {
+        const { name, description, price, category, quantity, shipping } = req.fields
+        const { photo } = req.files
+
+        switch (true) {
+            case !name.trim():
+                res.json({ error: "Name is required" });
+            case !description.trim():
+                res.json({ error: "description is required" })
+            case !price.trim():
+                res.json({ error: "Price is required" })
+            case !category.trim():
+                res.json({ error: "Category is required" })
+            case !quantity.trim():
+                res.json({ error: "Quantity is required" })
+            case !shipping.trim():
+                res.json({ error: "Shipping is required" })
+            case photo && photo.size > 100000:
+                res.json({ error: "Shipping is required" })
+        }
+
+        const product = new Product({ ...req.fields, slug: slugify(name) })
+
+        if (photo) {
+            product.photo.data = fs.readFileSync(photo.path);
+            product.photo.contentType = photo.type;
+        }
+
+        await product.save()
+        res.json(product)
 
     } catch (error) {
         return res.status(404).json(error)
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { requireSingin, isAdmin } from '../middlewares/auth.js';
 import { create, update, remove, list, read } from "../controllers/product.js"
+import formidable from "express-formidable"
 const router = express.Router();
 
 
-router.post('/product', requireSingin, isAdmin, create);
+router.post('/product', requireSingin, isAdmin, formidable(), create);
 router.put('/product/:productId', requireSingin, isAdmin, update);
 router.delete('/product/:productId', requireSingin, isAdmin, remove);
 router.get('/product/:slug', read)
