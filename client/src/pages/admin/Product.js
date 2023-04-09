a3b32b3aee9c4a4cb818be3c4d99eb9ab3e11159
Diogo Nóbrega <diogonrl@hotmail.com>
2023-04-09 02:55:22 -0300

    21 | Input photo

--- client/src/pages/admin/Product.js
+++ client/src/pages/admin/Product.js
@@ -1,6 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const AdminProduct = () => {
+  const [categories, setCategories] = useState([]);
+  const [photo, setPhoto] = useState("");
+  const [name, setName] = useState("");
+  const [descr, setDescr] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [shipping, setShipping] = useState("");
+  const [quantity, setQuantity] = useState("");
+
+  console.log(photo);
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = async () => {
+    try {
+      const { data } = await axios.get("/categories");
+      if (data) {
+        setCategories(data);
+      }
+    } catch (error) {}
+  };
+
   return (
     <div className="App">
       <NavLink className="nav-link" to="/dashboard/admin">
@@ -9,6 +35,37 @@ const AdminProduct = () => {
       <NavLink className="nav-link" to="/dashboard/admin/category">
         CreateCategory
       </NavLink>
+
+      <div>
+        <input
+          type="file"
+          name="photo"
+          accept="image/*"
+          onChange={(e) => setPhoto(e.target.files[0])}
+        />
+      </div>
+
+      {photo && (
+        <div>
+          <img
+            src={URL.createObjectURL(photo)}
+            alt="productPhoto"
+            className="img img-responsive"
+            height="100px"
+          />
+        </div>
+      )}
+
+      <select
+        onChange={(event) => {
+          setCategory(event.target.value);
+        }}
+      >
+        <option>Please choose one option</option>
+        {categories.map((option, index) => {
+          return <option key={index}>{option.name}</option>;
+        })}
+      </select>
     </div>
   );
 };
