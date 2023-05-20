4a423c8a3c1ce1e11075b3dad2e130440c376acd
Diogo Nóbrega <diogonrl@hotmail.com>
2023-05-20 15:51:18 -0300

    Loading and button pagination

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,18 +3,47 @@ import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 
 const Home = () => {
-  const [products, setProducts] = useState();
-
-  const loadProducts = async () => {
-    try {
-      const { data } = await axios.get("/products");
-      setProducts(data);
-    } catch (error) {}
-  };
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     loadProducts();
+    totalProducts();
   }, []);
+
+  useEffect(() => {
+    if (page === 1) return;
+    loadProductsPaginated();
+  }, [page]);
+
+  console.log(products && products.length < count);
+
+  const loadProductsPaginated = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`/products/${page}`);
+      setProducts([...products, ...data]);
+      setLoading(false);
+    } catch (error) {}
+  };
+
+  const loadProducts = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`/products/${page}`);
+      setProducts(data);
+      setLoading(false);
+    } catch (error) {}
+  };
+
+  const totalProducts = async () => {
+    try {
+      const { data } = await axios.get("/products-count ");
+      setCount(data);
+    } catch (error) {}
+  };
 
   return (
     <div>
@@ -23,6 +52,20 @@ const Home = () => {
           <ProductCard params={product} />
         </div>
       ))}
+      <div className="container-fluid">
+        {products && products.length < count && (
+          <button
+            disabled={loading}
+            onClick={(e) => {
+              e.preventDefault();
+              setPage(page + 1);
+            }}
+            className="btn btn-primary"
+          >
+            {loading ? "loading" : "load more"}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
