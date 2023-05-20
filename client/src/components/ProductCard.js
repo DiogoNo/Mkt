5ea13e02c3b705dae7d4a4ffbb5c428a9a5f30be
Diogo Nóbrega <diogonrl@hotmail.com>
2023-05-20 17:51:02 -0300

    Check if its list or single product

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, list }) => {
   const navigate = useNavigate();
   return (
     <div key={product._id}>
@@ -13,12 +13,14 @@ const ProductCard = ({ product }) => {
       />
       <div>{product.name}</div>
       <div className="d-flex justify-content-between">
-        <button
-          className="btn btn-primary col card-button"
-          onClick={() => navigate(`/product/${product.slug}`)}
-        >
-          View Product
-        </button>
+        {list && (
+          <button
+            className="btn btn-primary col card-button"
+            onClick={() => navigate(`/product/${product.slug}`)}
+          >
+            View Product
+          </button>
+        )}
         <button className="btn btn-secondary col card-button">AddCart</button>
       </div>
     </div>
