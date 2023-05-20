b96c8fad934723338e4735a029f65fc0978a099c
Diogo Nóbrega <diogonrl@hotmail.com>
2023-05-20 15:16:50 -0300

    Create endPoints

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -141,3 +141,27 @@ export const filteredProducts = async (req, res) => {
     res.status(400).json(error);
   }
 };
+
+export const productsCount = async (req, res) => {
+  try {
+    const total = await Product.find({}).estimatedDocumentCount();
+    res.json(total);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+export const productsPagination = async (req, res) => {
+  try {
+    const perPage = 6;
+    const page = req.params.page ? req.params.page : 1;
+
+    const products = await Product.find({})
+      .select('-photo')
+      .skip((page - 1) * perPage)
+      .limit(6)
+      .sort({ createdAt: -1 });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
